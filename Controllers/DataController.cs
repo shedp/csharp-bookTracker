d9f3f9dbfd13236f9b27f6a099dc261c56d4ea52
“shedp” <sheenadiep@gmail.com>
2023-09-14 11:08:16 +0100

    add links to navbar

---
+++ Controllers/DataController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using csharp_intro.Models;
+
+namespace csharp_intro.Controllers;
+
+public class DataController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
